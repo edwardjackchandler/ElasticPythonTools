7db910b1bb0af18f4528b92da82104be7663e5d1
edwardjackchandler <edwardjackchandler@gmail.com>
2018-10-19 10:33:27 +0100

    example updated old files deleted

--- example.py
+++ example.py
@@ -1,8 +1,8 @@
-import app.apl_elastic as apl_elastic
+import aiimi_python.ap_elastic as ap_elastic
 
 csv_location = "C:\\Users\\jchandler\\Documents\\Datasets\\Simpsons\\simpsons_characters.csv"
 
-apl = apl_elastic.AplElastic(host='localhost', port='9200')
+apl = ap_elastic.ApElastic(host='localhost', port='9200')
 
 characters_template = {
             "mappings": {
@@ -37,11 +37,11 @@ characters_template = {
 
 #apl.reindex(original_index_name="idx_simpsons_characters_with_mappings", target_index_name="idx_simpsons_characters_copy2", delete_target_index=True)
 
-#apl.reindex(original_index_name="idx_simpsons_characters", target_index_name="idx_simpsons_characters_copy2", delete_target_index=True)
+apl.reindex(original_index_name="idx_simpsons_characters", target_index_name="idx_simpsons_characters_copy2", delete_target_index=True)
 
-#apl.add_property_mapping(index_name="idx_simpsons_characters_copy2", type="character", property_name="race", property_type="keyword")
+apl.add_property_mapping(index_name="idx_simpsons_characters_copy2", type="character", property_name="race", property_type="keyword")
 
 #apl.add_property_value(index_name="idx_simpsons_characters_copy2", type="character", property_name="race", property_value="maggie", doc_id="AV6Q17Jq8ONIOQTCvcQs")
 
-apl.add_property_all_docs(index_name="idx_simpsons_characters_copy2", type="character", property_name="race", property_value="maggie")
+apl.add_property_all_docs(index_name="idx_simpsons_characters_copy2", type="character", property_name="name", property_value="apu_lol")
 
